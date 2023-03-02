8c3eb8e84ece7d60e0c57b073802f786a77967e2
wassim-hachani <hachaniwasssim10@gmail.com>
2023-03-02 11:45:50 +0100

    Dreamers Travel

--- script.js
+++ script.js
@@ -14,5 +14,56 @@ document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
        if(inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
     };
  });
+
+
+
+
+
+
+
+ /* ---------------------------------------------------------------------*/ 
+
+ const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+// Buttons
+const prevButton = document.querySelector(".carousel-prev");
+const nextButton = document.querySelector(".carousel-next");
+
+// Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+
+// Button listeners
+nextButton.addEventListener("click", () => {
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+prevButton.addEventListener("click", () => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+// Looping the carousel
+carouselSlide.addEventListener("transitionend", () => {
+  /*if (carouselImages[counter].id === "lastClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+  }*/
+  if (carouselImages[counter].id === "firstClone") {
+    carouselSlide.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+  }
+});
+
  
  
