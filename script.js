fcea226da492e1a382c718046e9829b417dfbcd6
wassim-hachani <hachaniwasssim10@gmail.com>
2023-03-11 16:17:28 +0100

    agency travel

--- script.js
+++ script.js
@@ -53,11 +53,12 @@ prevButton.addEventListener("click", () => {
 
 // Looping the carousel
 carouselSlide.addEventListener("transitionend", () => {
-  /*if (carouselImages[counter].id === "lastClone") {
+  if (carouselImages[counter].id === "lastClone") {
     carouselSlide.style.transition = "none";
     counter = carouselImages.length - 2;
     carouselSlide.style.transform = `translateX(${-size * counter}px)`;
-  }*/
+  }
+
   if (carouselImages[counter].id === "firstClone") {
     carouselSlide.style.transition = "none";
     counter = carouselImages.length - counter;
@@ -65,5 +66,9 @@ carouselSlide.addEventListener("transitionend", () => {
   }
 });
 
- 
+
+
+//-----------------------------------------------------------------------
+
+
  
